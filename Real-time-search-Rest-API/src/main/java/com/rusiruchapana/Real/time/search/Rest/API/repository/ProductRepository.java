6a0f8fe95a70be59e3476d66500599ccdb9b6b22
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-05-21 00:00:56 +0530

    Create JPQL queries in repository interface to get search results.

--- Real-time-search-Rest-API/src/main/java/com/rusiruchapana/Real/time/search/Rest/API/repository/ProductRepository.java
+++ Real-time-search-Rest-API/src/main/java/com/rusiruchapana/Real/time/search/Rest/API/repository/ProductRepository.java
@@ -2,6 +2,15 @@ package com.rusiruchapana.Real.time.search.Rest.API.repository;
 
 import com.rusiruchapana.Real.time.search.Rest.API.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product , Long> {
+
+    @Query("select p from Product p where "+ 
+            "p.name like concat('%',:query,'%')"+
+            "or  p.description like concat('%',:query,'%') ")
+    List<Product> searchProducts(String query);
+
 }
