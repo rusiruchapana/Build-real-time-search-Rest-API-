96c5649a05121e705212980e7ea971dcbebfc461
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-05-20 21:15:55 +0530

    Create product JPA entity.

---
+++ Real-time-search-Rest-API/src/main/java/com/rusiruchapana/Real/time/search/Rest/API/entity/product.java
@@ -0,0 +1,34 @@
+package com.rusiruchapana.Real.time.search.Rest.API.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String storeKeepingUnit;
+    private String name;
+    private String description;
+    private boolean active;
+    private String imgUrl;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime dateUpdated;
+
+}
